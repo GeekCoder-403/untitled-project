2c3c561d8d5583f678624edbf6dbf53ec516cd67
Sujit Maharana <sujit.maharana@vidyutva.com>
2025-04-26 13:40:42 +0530

    Latest update add component

---
+++ app/components/componentKit/FeatureCard.tsx
@@ -0,0 +1,86 @@
+import { Box, Card, CardActions, CardContent, Typography } from "@mui/material";
+
+interface FeatureCardProps {
+    title: string;
+    subTitle?: string;
+    description: string;
+    image: string;
+}
+
+const FeatureCard = ({ title, subTitle, description, image }: FeatureCardProps) => {
+    return (
+        <Card
+            variant="outlined"
+            sx={{
+                position: "relative",
+                backgroundColor: "white",
+                borderRadius: 2,
+                boxShadow: 1,
+                transition: "0.3s",
+                overflow: "hidden",
+                height: "100%",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
+                "&:hover": {
+                    backgroundColor: "#e6f6f7",
+                    boxShadow: 4,
+                },
+            }}
+        >
+            {/* Top Short Line */}
+            <Box
+                sx={{
+                    position: "absolute",
+                    top: 0,
+                    left: 16,
+                    width: "40px",
+                    height: "3px",
+                    backgroundColor: "#85bec3",
+                    borderTopLeftRadius: "2px",
+                    borderTopRightRadius: "2px",
+                }}
+            />
+
+            <CardContent>
+                <Box className="flex items-start justify-between">
+                    <Box>
+                        <Typography component="h2" color="black" fontWeight="bold">
+                            {title}
+                        </Typography>
+                        {subTitle && (
+                            <Typography className="text-xs text-gray-600">{subTitle}</Typography>
+                        )}
+                    </Box>
+
+                    <img src={image} alt="Icon" className="w-5 h-5" />
+                </Box>
+
+                <Typography variant="body2" color="text.secondary" sx={{ mt: 1 }}>
+                    {description}
+                </Typography>
+            </CardContent>
+
+            <CardActions className="flex justify-between items-center mt-8">
+                <Box className="flex items-center gap-1">
+                    <img
+                        src="/icons/Download.svg"
+                        alt="Download"
+                        className="w-4 h-4 cursor-pointer transition-all duration-300 filter group-hover:brightness-0 group-hover:invert"
+                    />
+                    <Typography className="text-sm text-gray-800">
+                        Download Reference
+                    </Typography>
+                </Box>
+
+                <img
+                    src="/icons/MoveRight.svg"
+                    alt="Move Right"
+                    className="w-4 h-4 text-gray-800 cursor-pointer"
+                />
+            </CardActions>
+        </Card>
+    );
+};
+
+export default FeatureCard;
--- app/routes/_control-center/admin/home-admin/route.tsx
+++ app/routes/_control-center/admin/home-admin/route.tsx
@@ -1,5 +1,5 @@
-import { Box, Card, CardActions, CardContent, Grid2, Typography } from "@mui/material";
-import { Activity, BarChart3, Map, Database, Settings, Share2 } from "lucide-react";
+import { Box, Grid2 } from "@mui/material";
+import FeatureCard from "~/components/componentKit/FeatureCard";
 
 const cardData = [
     {
@@ -39,72 +39,16 @@ const cardData = [
 const route = () => (
     <Box sx={{ padding: 2, display: "flex", flexDirection: "column", gap: 2 }}>
         <Grid2 container spacing={2}>
-            {cardData.map((card, index) => {
-                const imageComponent = card.image;
-
-                return (
-                    <Grid2 key={index} size={{ xs: 12, sm: 6, md: 4, lg: 3 }}>
-                        <Card
-                            variant="outlined"
-                            sx={{
-                                position: "relative",
-                                backgroundColor: "white",
-                                borderRadius: 2,
-                                boxShadow: 1,
-                                transition: "0.3s",
-                                overflow: "hidden",
-                                "&:hover": {
-                                    backgroundColor: "#e6f6f7",
-                                    boxShadow: 4,
-                                },
-                                height: "100%",
-                                display: "flex",
-                                flexDirection: "column",
-                                justifyContent: "space-between",
-                            }}
-                        >
-                            <Box
-                                sx={{
-                                    position: "absolute",
-                                    top: 0,
-                                    left: 16,
-                                    width: "40px",
-                                    height: "3px",
-                                    backgroundColor: "#85bec3",
-                                    borderTopLeftRadius: "2px",
-                                    borderTopRightRadius: "2px",
-                                }}
-                            />
-
-                            <CardContent>
-                                <Box className="flex items-start justify-between">
-                                    <Box>
-                                        <Typography component="h2" color="black" fontWeight="bold" >
-                                            {card.title}
-                                        </Typography>
-                                        {card.subTitle && (
-                                            <Typography className="text-xs text-gray-600">{card.subTitle}</Typography>
-                                        )}
-                                    </Box>
-                                    <img src={imageComponent} alt="Loading..." className="w-5 h-5" />
-                                </Box>
-
-                                <Typography variant="body2" color="text.secondary" sx={{ mt: 1 }}>
-                                    {card.description}
-                                </Typography>
-                            </CardContent>
-
-                            <CardActions className="flex justify-between items-center mt-8">
-                                <Box className="flex items-center gap-1 ">
-                                    <img src="/icons/Download.svg" alt="Loading..." className="w-4 h-4 cursor-pointer transition-all duration-300 filter group-hover:brightness-0 group-hover:invert" />
-                                    <Typography className="text-sm text-gray-800">Download Reference</Typography>
-                                </Box>
-                                <img src="/icons/MoveRight.svg" alt="Loading..." className="w-4 h-4 text-gray-800 cursor-pointer" />
-                            </CardActions>
-                        </Card>
-                    </Grid2>
-                );
-            })}
+            {cardData.map((card, index) => (
+                <Grid2 key={index} size={{ xs: 12, sm: 6, md: 4, lg: 3 }}>
+                    <FeatureCard
+                        title={card.title}
+                        subTitle={card.subTitle}
+                        description={card.description}
+                        image={card.image}
+                    />
+                </Grid2>
+            ))}
         </Grid2>
     </Box>
 );
